51ddbd7961e88af287afc5671b75cbf36732eb6a
maka <maka>
2024-07-23 15:45:14 +0600

    fix MainActivity

--- app/src/main/java/com/example/testandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/testandroidapp/MainActivity.kt
@@ -17,32 +17,6 @@ import com.example.testandroidapp.ui.theme.TestAndroidAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TestAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestAndroidAppTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_login)
     }
 }
